b51bf55811c4ac3256b6909a5e7413a91abad0b5
Kira Palii <kirapaliy80@gmail.com>
2025-05-19 17:50:11 +0300

    The first implementation of the site

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function toggleDropdownMenu() {
+    document.getElementById('dropdownMenu').classList.toggle('hidden');
+}
+function toggleDropdownTheme() {
+    document.getElementById('dropdownTheme').classList.toggle('hidden');
+}
