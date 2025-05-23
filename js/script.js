5bb8875b83b5fb875538894aa458fee065fcc3bd
Kira Palii <kirapaliy80@gmail.com>
2025-05-23 17:38:04 +0300

    Implementation of all themes on the site

--- js/script.js
+++ js/script.js
@@ -4,3 +4,16 @@ function toggleDropdownMenu() {
 function toggleDropdownTheme() {
     document.getElementById('dropdownTheme').classList.toggle('hidden');
 }
+
+function setTheme(themeName) {
+    const html = document.documentElement;
+    html.classList.remove('theme-w-red', 'theme-w-blue', 'theme-w-brown', 'theme-w-green', 'theme-w-purple', 'theme-b-red', 'theme-b-blue', 'theme-b-brown', 'theme-b-green', 'theme-b-purple');
+    html.classList.add('theme-' + themeName);
+    localStorage.setItem('theme', themeName);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const savedTheme = localStorage.getItem("theme") || "w-red";
+    setTheme(savedTheme);
+  });
+  
